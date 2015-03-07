b2024574f7bf64b62fbebd0756371e71496dc371
Magdalena Petrova <magik70@abv.bg>
2015-03-07 15:55:43 +0200

    07_Homework_AssertionsAndExceptions

---
+++ 07_Homework_AssertionsAndExceptions/Assertions-and-Exceptions/Exceptions/CSharpExam.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class CSharpExam : Exam
+{
+    private int score;
+
+    public CSharpExam(int score)
+    {
+        this.Score = score;
+    }
+
+    public int Score 
+    {
+        get
+        {
+            return this.score;
+        }
+        private set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Score cannot be negative.");
+            }
+
+            if (value > 100)
+            {
+                throw new ArgumentException("Score cannot be greater than 100.");
+            }
+
+            this.score = value;
+        } 
+    }
+
+    public override ExamResult Check()
+    {
+        return new ExamResult(this.Score, 0, 100, "Exam results calculated by score.");
+    }
+}
---
+++ 07_Homework_AssertionsAndExceptions/Assertions-and-Exceptions/Exceptions/Exceptions.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Exceptions
+{
+    public static T[] Subsequence<T>(T[] arr, int startIndex, int count)
+    {
+        if (startIndex >= arr.Length)
+        {
+            throw new ArgumentOutOfRangeException("StartIndex cannot be greater than Array Length.");
+        }
+
+        if (count <= 0)
+        {
+            throw new ArgumentOutOfRangeException("Count cannot be negative.");
+        }
+
+        List<T> result = new List<T>();
+        for (int i = startIndex; i < startIndex + count; i++)
+        {
+            result.Add(arr[i]);
+        }
+        return result.ToArray();
+    }
+
+    public static string ExtractEnding(string str, int count)
+    {
+        if (count > str.Length)
+        {
+            throw new ArgumentOutOfRangeException("Count cannot be greater than String Length.");
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = str.Length - count; i < str.Length; i++)
+        {
+            result.Append(str[i]);
+        }
+        return result.ToString();
+    }
+
+    public static void CheckPrime(int number)
+    {
+        for (int divisor = 2; divisor <= Math.Sqrt(number); divisor++)
+        {
+            if (number % divisor == 0)
+            {
+                Console.WriteLine("The number {0} is not prime.", number);
+                return;
+            }
+        }
+
+        Console.WriteLine("The number {0} is prime.", number);
+    }
+
+    static void Main()
+    {
+        try
+        {
+            var substr = Subsequence("Hello!".ToCharArray(), 2, 3);
+            Console.WriteLine(substr);
+
+            var subarr = Subsequence(new int[] { -1, 3, 2, 1 }, 0, 2);
+            Console.WriteLine(String.Join(" ", subarr));
+
+            var allarr = Subsequence(new int[] { -1, 3, 2, 1 }, 0, 4);
+            Console.WriteLine(String.Join(" ", allarr));
+
+            var emptyarr = Subsequence(new int[] { -1, 3, 2, 1 }, 0, 0);
+            Console.WriteLine(String.Join(" ", emptyarr));
+        }
+        catch (ArgumentOutOfRangeException aorEx)
+        {
+            Console.WriteLine(aorEx.Message);
+        }
+
+        try
+        {
+            Console.WriteLine(ExtractEnding("I love C#", 2));
+            Console.WriteLine(ExtractEnding("Nakov", 4));
+            Console.WriteLine(ExtractEnding("beer", 4));
+            Console.WriteLine(ExtractEnding("Hi", 100));
+        }
+        catch(ArgumentOutOfRangeException aorEx)
+        {
+            Console.WriteLine(aorEx.Message);
+        }
+
+
+        CheckPrime(23);
+        CheckPrime(33);
+
+        List<Exam> peterExams = new List<Exam>()
+        {
+            new SimpleMathExam(2),
+            new CSharpExam(55),
+            new CSharpExam(100),
+            new SimpleMathExam(1),
+            new CSharpExam(0),
+        };
+
+        Student peter = new Student("Peter", "Petrov", peterExams);
+        double peterAverageResult = peter.CalcAverageExamResultInPercents();
+        Console.WriteLine("{0}'s average results = {1:p0}",peter.FirstName, peterAverageResult);
+
+        Student ivan = new Student("Ivan", "Ivanov");
+        double ivanAverageResult = ivan.CalcAverageExamResultInPercents();
+        Console.WriteLine("{0}'s average results = {1:p0}",ivan.FirstName, ivanAverageResult);
+    }
+}
